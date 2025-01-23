a79b259a3ee360d63608bde374746bba4f75ca1d
naveen123-rocks <140931221+naveen123-rocks@users.noreply.github.com>
2025-01-23 21:52:57 +0530

    newuploaded

---
+++ luein_hire/candidate.js
@@ -0,0 +1,10 @@
+
+
+  const toggleButton = document.querySelector('.menu-toggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  toggleButton.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+
+
